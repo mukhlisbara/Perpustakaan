63affbdc890b805092c640be880d90ae1cf54963
mukhlisbara <mukhlisbara12@gmail.com>
2022-07-17 16:16:56 +0700

    sistem perpustakaan

---
+++ cover.php
@@ -0,0 +1,50 @@
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" name="viewport">
+    <title>Perpustakaan</title>
+    <link rel="shortcut icon" href="gambar/logo_login.png">
+    <link rel="stylesheet" href="https://getstisla.com/dist/modules/prism/prism.css">
+    <link rel="stylesheet" href="https://getstisla.com/dist/modules/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://getstisla.com/dist/modules/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="https://getstisla.com/dist/modules/chocolat/dist/css/chocolat.css">
+    <link rel="stylesheet" href="https://getstisla.com/dist/css/style.css">
+    <link rel="stylesheet" href="https://getstisla.com/dist/css/custom.css">
+    <link rel="stylesheet" href="https://getstisla.com/landing/style.css">
+</head>
+
+<body class="" >
+  <nav class="navbar navbar-reverse navbar-expand-lg">
+      <div class="container">
+          <a class="navbar-brand smooth" href="cover.php">Perpustakaan Online</a>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ml-auto align-items-lg-center d-none d-lg-block">
+                  <li class="ml-lg-3 nav-item">
+                      <a href="login.php" class="btn btn-round smooth btn-icon icon-left" >LOGIN</a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+  </nav>
+  
+  <div class="hero-wrapper" id="top"> 
+    <div class="hero">
+        <div class="container">
+            <div class="text text-center text-lg-left">
+                <h1>Perpustakaan</h1>
+                <p class="lead">
+                  Perpustakaan  tempat dimana banyak menyimpan berbagai macam buku  ilmu pengetahuan yang banyak bermanfaat. Perpustakaan dimana tempat  
+                  pelajar dan mahasiswa banyak melakukan aktivitas membaca, dengan ruang yang hening jauh dari kebisingan yang mampu menciptakan ruang 
+                  membaca yang begitu nyaman bagi mereka yang gemar membaca.
+                </p>
+                <div class="cta">
+                    <a class="btn btn-lg btn-warning btn-icon icon-right" href="login.php">MULAI MEMBACA ></a> &nbsp;
+                </div>
+            </div>
+            <div class="image d-none d-lg-block">
+                <img src="gambar/buku.png" alt="img">
+            </div>
+        </div>
+    </div>
+  </div>
+</body>    
